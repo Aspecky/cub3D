6bd9503d3debf53db57883f2655e943b3f4c3a0e
Aspecky <tarrihmoad@proton.com>
2026-01-10 21:30:40 +0100

    fix parsing

--- source/src/parsing/border_checker.c
+++ source/src/parsing/border_checker.c
@@ -3,24 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   border_checker.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kaneddam <kaneddam@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: mtarrih <mtarrih@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 17:00:00 by kaneddam          #+#    #+#             */
-/*   Updated: 2026/01/08 16:00:30 by kaneddam         ###   ########.fr       */
+/*   Updated: 2026/01/10 21:25:47 by mtarrih          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
-
-int	find_first_non_space(char *line)
-{
-	int	i;
-
-	i = 0;
-	while (line[i] && line[i] == ' ')
-		i++;
-	return (i);
-}
 
 int	find_last_non_space(char *line)
 {
@@ -64,26 +54,21 @@ int	check_side_borders(t_parsing *data, int i, int first, int last)
 	return (1);
 }
 
+static int	is_outside_map(t_parsing *data, int x, int y)
+{
+	if (y < 0 || y >= data->map_height)
+		return (1);
+	if (x < 0 || x >= (int)ft_strlen(data->map[y]))
+		return (1);
+	return (0);
+}
+
 int	check_space_leaks(t_parsing *data, int i, int j)
 {
 	if (data->map[i][j] != '1' && data->map[i][j] != ' ')
 	{
-		if (get_cell(data, j, i - 1) == ' ')
-		{
-			dputstr("Error: map must be surrounded by walls\n", 2);
-			return (0);
-		}
-		if (get_cell(data, j, i + 1) == ' ')
-		{
-			dputstr("Error: map must be surrounded by walls\n", 2);
-			return (0);
-		}
-		if (get_cell(data, j - 1, i) == ' ')
-		{
-			dputstr("Error: map must be surrounded by walls\n", 2);
-			return (0);
-		}
-		if (get_cell(data, j + 1, i) == ' ')
+		if (is_outside_map(data, j, i - 1) || is_outside_map(data, j, i + 1)
+			|| is_outside_map(data, j - 1, i) || is_outside_map(data, j + 1, i))
 		{
 			dputstr("Error: map must be surrounded by walls\n", 2);
 			return (0);
